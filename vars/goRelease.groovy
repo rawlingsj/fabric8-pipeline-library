b8df869857dd39955799763a4c37b997f83280b8
James Rawlings <rawlingsj80@gmail.com>
2017-05-23 13:54:12 +0100

    Update goRelease.groovy

--- vars/goRelease.groovy
+++ vars/goRelease.groovy
@@ -29,7 +29,7 @@ def call(body) {
     sh "mkdir -p ${buildPath}"
 
     dir(buildPath) {
-        git url: "https://github.com/rawlingsj/${prj}.git", branch: 'hack'
+        git url: "https://github.com/rawlingsj/${prj}.git", branch: 'work'
 
         sh "git config user.email fabric8-admin@googlegroups.com"
         sh "git config user.name fabric8-release"
